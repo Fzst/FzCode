f3130d8fd1349950a6200a15c3e49880efcdf1b1
Fzst <1506408707@qq.com>
2020-11-30 20:39:35 +0800

    gp

--- Graph.cpp
+++ Graph.cpp
@@ -9,8 +9,61 @@ typedef struct{
 	int arcs[MAXN][MAXN];
 	int vexnum,arcnum;
 }ABGraph;
-
+typedef struct QNode{
+    char data;
+    struct QNode *next;
+}QNode,*Queue;
+typedef struct{
+    Queue front;
+    Queue rear;
+}LinkQueue;
 int visited[MAXN];
+
+void Init_Queue(LinkQueue *q){
+    q->front = q->rear = (Queue)malloc(sizeof(QNode));
+    q->front->next = NULL;
+}
+
+void In_Queue(LinkQueue *q,char x){
+    Queue p = (Queue)malloc(sizeof(QNode));
+    p->data = x;
+    p->next = NULL;
+    q->rear->next = p;
+    q->rear = p;
+}
+void De_Queue(LinkQueue *q,char &x){
+ Queue p;
+ if(q->front==q->rear);
+ else
+ {
+  p = q->front->next;
+  x = p->data;
+  q->front->next = p->next;
+  if(q->rear==p)
+   q->rear=q->front;
+  free(p);
+ }
+}
+int Empty_Queue(LinkQueue *q){
+ if(q->front==q->rear)
+  return 1;
+ else
+  return 0;
+}
+int First_AdjVex(ABGraph *g,int c){
+ int j;
+ for(j=0;j<g->vexnum;j++)
+  if(g->arcs[c][j]<MAXNUM && visited[j]==0)
+   return j;
+  return -1;
+}
+int Next_AdjVex(ABGraph *g,int c,int w){
+ int j;
+ for(j=0;j<g->vexnum;j++)
+  if(g->arcs[c][j]<MAXNUM && visited[j]==0)
+   return j;
+  return -1;
+}
 int LocateVex(ABGraph *g,char x){
 	int i;
 	for(i = 0;i < g->vexnum;i++){
@@ -59,16 +112,38 @@ void DFS_Graph(ABGraph *g,char x){
 		DFS_Graph(g,g->vex[i]);
 	}
 }
+void BFS_Graph(ABGraph *g,char v){
+ int c,w,i;
+ char u;
+ LinkQueue *Q ;
+ c = Visit(g,v);
+ visited[c] = 1;
+ Init_Queue(Q);
+ In_Queue(Q,v);
+ while(!Empty_Queue(Q)){
+  De_Queue(Q,u);
+  c = LocateVex(g,u);
+  for(w=First_AdjVex(g,c);w>=0;w=Next_AdjVex(g,c,w))
+  {
+   if(!visited[w]){
+    i = Visit(g,g->vex[w]);
+    visited[i] = 1;
+    In_Queue(Q,g->vex[w]);
+   }
+  }
+ }
+}
 int main(){
 	ABGraph *g = (ABGraph *)malloc(sizeof(ABGraph));
 	char x;
 	for(int i = 0;i < g->vexnum;i++)
 		visited[i] = 0;
 	Creat_Graph(g);
-	getchar(); 
+	getchar();
 	printf("请输入要开始搜索的顶点：\n");
 	scanf("%c",&x);
-	DFS_Graph(g,x);
+	//DFS_Graph(g,x);
+	BFS_Graph(g,x);
 	return 0;
 }
 
